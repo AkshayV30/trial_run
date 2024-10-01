a802428cb4468bc1bb4203b354257d7f366bacba
akshay <akshayatwork.v30@gmail.com>
2024-10-01 20:50:15 +0530

    test partial working 0.1

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -1,9 +1,16 @@
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
+
+// 4
+const allowedOrigins = [
+  "http://localhost:4200", // Angular development server
+  "http://localhost:4000", // Angular SSR server
+  "http://frontend:4000", // Production URL
+];
 
 export const corsMiddleware = (req, res, next) => {
   // 1
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
   // 2
   // const allowedOrigin = process.env.CORS_ORIGIN;
   // 3
@@ -11,17 +18,16 @@ export const corsMiddleware = (req, res, next) => {
   // process.env.NODE_ENV === "production"
   //   ? "http://frontend:4000" // Production URL
   //   : "http://localhost:4200"; // Development URL
+
   // 4
-  // const allowedOrigin = [
-  //   "http://localhost:4200", // Angular development server
-  //   "http://localhost:4000", // Angular SSR server
-  //   "http://frontend:4000", // Production URL
-  // ];
+  const origin = req.headers.origin;
 
-  // res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE,OPTIONS"); // Allow specific HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow specific headers
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     return res.status(204).end();
